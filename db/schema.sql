c9644cf1a4974b7ecf49a31bf4bb15fc8352c608
Daniel <ddoherty6@gmail.com>
2022-01-26 16:09:46 -0800

    added sql schema

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employee_tracker;
+CREATE DATABASE employee_tracker;
+USE employee_tracker;
+
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER,
+    CONSTRAINT department_id_fk FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER,
+    CONSTRAINT role_id_fk FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL
+);
+
+
---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO department (name)
+VALUES
+('Sales'),
+('Marketing'),
+('Information Technology'),
+('Corporate Services'),
+('Finance'),
+('Consulting');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('CSO', '500000', '1'),
+('Account Manager', '125000', '1'),
+('CMO', '500000', '2'),
+('Content Marketing Specialist', '90000', '2'),
+('Lead Salesforce Technician', '150000', '3'),
+('Database Engineer', '75000', '3'),
+('Building Coordinator', '65000', '4'),
+('Lead Security', '90000', '4'),
+('CSO', '500000', '5'),
+('Lead Accountant', '175000', '5'),
+('Senior Consultant', '250000', '6'),
+('Junior Consultant', '80000', '6');
